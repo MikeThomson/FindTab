a661b132536e36d1f173a4deef606f048a573763
Mike Thomson <mike@bluetempest.net>
2012-08-04 01:01:13 -0500

    doesn't work, but can focus the right tab

--- FindTabs.js
+++ FindTabs.js
@@ -5,23 +5,34 @@ var FindTab = (function() {
 	};
 
 	var getTabsCallback = function(tabs) {
-		alert('starting');
 		for(i in tabs) {
 			document.body.innerHTML = document.body.innerHTML + "<p>" + tabs[i].url + "</p>";
+			goToTab(tabs[i]);
 		}
 	}
 
 	var getTabs = function() {
 		chrome.tabs.query({
-				url: '*://*/*'
+				url: '*://movies.netflix.com/*'
 			}, getTabsCallback
 		);
 	};
 
+	var goToTab = function(tab) {
+		alert('going to ');
+		chrome.windows.update(tab.windowId, {
+			focused : true
+		});
+		chrome.tabs.update(tab.id, {
+			active : true
+		});
+	}
+
 	return {
 		getFoundTabList : function() {
 			getTabs();
-		}
+		},
+
 	}
 
 })();
